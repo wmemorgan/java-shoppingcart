f9e6beec41de2ed6ca900b8a51f2ad90752d70fe
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-27 01:05:08 +0800

    Add '/users/myinfo' endpoint

--- shoppingcart/src/main/java/com/lambdaschool/shoppingcart/controllers/UserController.java
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,6 +43,12 @@ public class UserController
         User u = userService.findUserById(userId);
         return new ResponseEntity<>(u,
                                     HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/myinfo", produces = {"application/json"})
+    public ResponseEntity<?> getUserInfo(Authentication authentication) {
+        User u = userService.findByName(authentication.getName());
+        return new ResponseEntity<>(u, HttpStatus.OK);
     }
 
     @PostMapping(value = "/user", consumes = {"application/json"})
